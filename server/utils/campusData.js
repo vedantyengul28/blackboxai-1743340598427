1000aaf8c1d6b6522cdc6865cd50ee92526fed7b
Vedant Yengul <165377916+vedantyengul28@users.noreply.github.com>
2025-03-30 18:47:36 +0530

    Add server/utils/campusData.js 1743340656274

---
+++ server/utils/campusData.js
@@ -0,0 +1,25 @@
+const campuses = [
+  'Harvard University',
+  'Stanford University',
+  'MIT',
+  'University of California, Berkeley',
+  'Columbia University',
+  'Yale University',
+  'Princeton University',
+  'University of Chicago',
+  'Cornell University',
+  'University of Pennsylvania'
+];
+
+const categories = [
+  'Textbooks',
+  'Electronics',
+  'Furniture',
+  'Clothing',
+  'Sports Equipment',
+  'Stationery',
+  'Kitchenware',
+  'Other'
+];
+
+module.exports = { campuses, categories };
